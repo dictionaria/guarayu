fb00bb0b1aac3cea6734ba416fffd8015510a056
Johannes Englisch <englisch@shh.mpg.de>
2021-02-04 13:00:12 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_guarayu',
+    py_modules=['cldfbench_guarayu'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'guarayu=cldfbench_guarayu:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
